8042e7923818a16c7b913ffaaa9d0a17b1e476d2
gap <tyehcwc402004@gmail.com>
2024-12-06 00:26:40 +0800

    fix

--- frontendv3/src/main.tsx
+++ frontendv3/src/main.tsx
@@ -1,9 +1,8 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
-import { PersistGate } from 'redux-persist/integration/react';
 import App from "./App"
-import { persistor ,store } from "./app/store"
+import { store } from "./app/store"
 import "./index.css"
 import { BrowserRouter } from "react-router-dom"
 
@@ -15,11 +14,7 @@ if (container) {
   root.render(
     <React.StrictMode>
       <Provider store={store}>
-        {/* <BrowserRouter> */}
-        {/* <PersistGate loading={null} persistor={persistor}> */}
           <App />
-          {/* </PersistGate> */}
-        {/* </BrowserRouter> */}
       </Provider>
     </React.StrictMode>,
   )
